59934fe25dbaf5c907fb8b74a0924fe17a0db7fe
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2023-10-04 21:05:57 -0500

    Updated file

--- src/app/typingtest/page.tsx
+++ src/app/typingtest/page.tsx
@@ -3,10 +3,10 @@
 import React, { useState } from 'react'
 
 const TypingTest = () => {
-  const [prompt, setPrompt] = useState('') 
-  const [untypedWords, setUntypedWords] = useState(['']) 
+  const [prompt, setPrompt] = useState('')
+  const [untypedWords, setUntypedWords] = useState([''])
   const [completedWords, setCompletedWords] = useState([''])
-  const [inputValue, setInputValue] = useState('')  
+  const [inputValue, setInputValue] = useState('')
   const [started, setStarted] = useState(false)
   const [startTime, setStartTime] = useState(0)
   const [timeElapsed, setTimeElapsed] = useState(0)
@@ -14,7 +14,7 @@ const TypingTest = () => {
   const [wpm, setWpm] = useState(0)
 
   /* Get the prompt of what we want the user to type here from MongoDB here */
-    const SetPrompt = () => {
+  const SetPrompt = () => {
     const prompt = `The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.`
     setPrompt(prompt)
     setUntypedWords(prompt.split(' '))
@@ -34,16 +34,18 @@ const TypingTest = () => {
     setStarted(true)
     setCompleted(false)
     setStartTime(Date.now())
+    setInterval(calculateWPM, 1000)
   }
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement> ) => {
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value
     const lastLetter = inputValue[inputValue.length - 1]
     const currentWord = untypedWords[0]
 
     if (lastLetter === ' ' || lastLetter === '.') {
-      if (inputValue.includes(currentWord)) {   // current word has been typed
-        const newWords = [...untypedWords.slice(1)]  
+      if (inputValue.includes(currentWord)) {
+        // current word has been typed
+        const newWords = [...untypedWords.slice(1)]
         const newCompletedWords = [...completedWords, currentWord]
         setUntypedWords(newWords)
         setCompletedWords(newCompletedWords)
@@ -53,13 +55,13 @@ const TypingTest = () => {
     } else {
       setInputValue(inputValue)
     }
-    calculateWPM()
   }
 
   const calculateWPM = () => {
     const now = Date.now()
     const diff = (now - startTime) / 1000 / 60
-    const wordsTyped = Math.ceil(completedWords.reduce((acc, word) => (acc += word.length), 0) / 5)
+    console.log('Bowser', completedWords.length)
+    const wordsTyped = completedWords.length
     const wpm = Math.ceil(wordsTyped / diff)
     setWpm(wpm)
     setTimeElapsed(diff)
@@ -69,62 +71,64 @@ const TypingTest = () => {
 
   return (
     <div id="body" className="flex h-screen content-center items-center justify-center bg-gray-900">
-      <div id="Wrapper" className="w-1/2 rounded-lg bg-gray-700 p-8 text-white aspect-video" onClick={focusInput}>
+      <div id="Wrapper" className="aspect-video w-1/2 rounded-lg bg-gray-700 p-8 text-white" onClick={focusInput}>
         <input
           id="input"
-          className="opacity-0 absolute"
+          className="absolute opacity-0"
           type="text"
           autoFocus={true}
-          autoComplete='off'
+          autoComplete="off"
           onChange={handleInput}
           value={inputValue}
         />
-        <div id="content-box" className="rounded-lg border-2 border-slate-600 p-4 aspect-video">
+        <div id="content-box" className="aspect-video rounded-lg border-2 border-slate-600 p-4">
           <div id="typing-test" className="no-scrollbar max-h-60 overflow-y-auto">
-            {!started || completed ? 'Press start to start' : prompt.split(' ').map((word, w_idx) => {
-              let highlight = false
-              let currentWord = false
+            {!started || completed
+              ? 'Press start to start'
+              : prompt.split(' ').map((word, w_idx) => {
+                  let highlight = false
+                  let currentWord = false
 
-              if (completedWords.length > w_idx) {
-                highlight = true
-              }
+                  if (completedWords.length > w_idx) {
+                    highlight = true
+                  }
 
-              if (completedWords.length === w_idx) {
-                currentWord = true
-              }
+                  if (completedWords.length === w_idx) {
+                    currentWord = true
+                  }
 
-              return (
-                <>
-                  {' '}
-                  <span
-                    key={w_idx}
-                    className={`
+                  return (
+                    <>
+                      {' '}
+                      <span
+                        key={w_idx}
+                        className={`
                                 ${highlight && 'text-green-600'} 
                                 ${currentWord && 'underline'}`}
-                  >
-                    {word.split('').map((letter, l_idx) => {
-                      const isCurrentWord = w_idx === completedWords.length
-                      const isWronglyTyped = letter !== inputValue[l_idx]
-                      const shouldBeHighlighted = l_idx < inputValue.length
-                      return (
-                        <span
-                          className={`letter ${
-                            isCurrentWord && shouldBeHighlighted
-                              ? isWronglyTyped
-                                ? 'text-red-600'
-                                : 'text-green-600'
-                              : ''
-                          }`}
-                          key={l_idx}
-                        >
-                          {letter}
-                        </span>
-                      )
-                    })}
-                  </span>
-                </>
-              )
-            })}
+                      >
+                        {word.split('').map((letter, l_idx) => {
+                          const isCurrentWord = w_idx === completedWords.length
+                          const isWronglyTyped = letter !== inputValue[l_idx]
+                          const shouldBeHighlighted = l_idx < inputValue.length
+                          return (
+                            <span
+                              className={`letter ${
+                                isCurrentWord && shouldBeHighlighted
+                                  ? isWronglyTyped
+                                    ? 'text-red-600'
+                                    : 'text-green-600'
+                                  : ''
+                              }`}
+                              key={l_idx}
+                            >
+                              {letter}
+                            </span>
+                          )
+                        })}
+                      </span>
+                    </>
+                  )
+                })}
           </div>
           <div id="content" className="mt-4 flex-col justify-between border-t-2 border-gray-600 py-4">
             <ul id="result-details" className="flex-col justify-between">
