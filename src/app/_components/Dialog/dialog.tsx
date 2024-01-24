d7a77a1e1474550a01ffd1891b4b6fd2cb014374
alexis-ng <alexism.ng26@gmail.com>
2024-01-24 11:00:46 -0600

    margins

--- src/app/_components/Dialog/dialog.tsx
+++ src/app/_components/Dialog/dialog.tsx
@@ -20,8 +20,8 @@ const Dialog = () => {
           <img className="h-auto max-w-sm rounded-lg shadow-xl dark:shadow-gray-800" src="/barista.png" alt="image description"></img>
       </div>
       {/* The dialog text */}
-      <div className='fixed bottom-0 left-0 z-50 w-full'>
-        <div className=" m-auto block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <div className='fixed bottom-0 left-0 z-50 my-10 mx-40'>
+        <div className=" m-auto block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Person's Name</h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus quis, dapibus rutrum lorem. Proin accumsan ex nisi, imperdiet mattis metus hendrerit vitae. Phasellus porttitor hendrerit eros, a varius lacus sollicitudin eu. In mi nulla, suscipit et nunc vitae, sagittis pulvinar nisl. Sed convallis egestas ligula, vitae elementum tellus pharetra non. Curabitur ac ultrices nisi, sit amet sodales mauris. In fringilla arcu in aliquet luctus. </p>
         </div>
