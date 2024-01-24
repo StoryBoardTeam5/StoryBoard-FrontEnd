bc34402695c13b914bc34a764d1a057ec80a7ab9
Andres Lopez-Bormann <75829728+AndresLopezBormann@users.noreply.github.com>
2024-01-24 11:20:14 -0600

    Add decision html and css

--- src/app/_components/Decision/decision.tsx
+++ src/app/_components/Decision/decision.tsx
@@ -4,6 +4,7 @@ Description   : Decision Modal for StoryBoard
 LastEditBy    : Andres Lopez-Bormann
 CreatedDate   : 2023-11-05
 Revisions :
+  2024-01-24 - Add Decision HTML and CSS
 Preconditions: N/A
 Postconditions: Decision page is rendered
 */
@@ -12,8 +13,38 @@ import React from 'react'
 
 const Decision = () => {
   return (
-    <div className='flex h-screen-with-nav'>
-      <h1 className='m-auto text-8xl'>Decision</h1>
+    <div>
+      <div
+        className='min-h-screen -z-50'
+        style={{
+          backgroundImage: 'url(/cafe.png)',
+        }}
+      />
+      <div className='fixed bottom-32 left-1/2 transform -translate-x-1/2  flex w-3/4 content-center items-center justify-center'>
+        <div className='m-auto block rounded-lg border border-gray-200 bg-white p-6 opacity-95 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+          <p className='pt-12 font-normal text-gray-700 dark:text-gray-200'>
+            The Barista gives you a choice of Coffee, You can have a latte for $12380.01 or a frap for $23892.1 Which do
+            you choose? Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus
+            quis, dapibus rutrum lorem. Proin accumsan ex nisi, imperdiet mattis metus hendrerit vitae. Phasellus
+            porttitor hendrerit eros, a varius lacus sollicitudin eu. In mi nulla, suscipit et nunc vitae, sagittis
+            pulvinar nisl. Sed convallis egestas ligula, vitae elementum tellus pharetra non. Curabitur ac ultrices
+            nisi, sit amet sodales mauris. In fringilla arcu in aliquet luctus.
+          </p>
+          <h5 className='m-auto pt-12 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+            What do you want to drink?
+          </h5>
+          <div className='flex flex-row justify-evenly p-16'>
+            <button className='m-auto w-1/3 rounded-lg border border-gray-200 bg-white p-6 opacity-95 shadow hover:bg-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-900'>
+              Choice 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus
+              quis
+            </button>
+            <button className='m-auto block w-1/3 rounded-lg border border-gray-200 bg-white p-6 opacity-95 shadow hover:bg-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-900'>
+              Choice 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus
+              quis
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
