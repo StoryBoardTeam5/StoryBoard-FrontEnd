eea3e2bc4b11d542e5f89ab69a870b5a6d1680e0
alexis-ng <alexism.ng26@gmail.com>
2024-02-02 09:35:11 -0600

    readded cafe and added typing animation

--- src/app/_components/Dialog/dialog.tsx
+++ src/app/_components/Dialog/dialog.tsx
@@ -9,20 +9,23 @@ Postconditions: Dialog page is rendered
 */
 
 import React from 'react'
+import Typewriter from "./Typewriter";
 const Dialog = () => {
   return (
     <div className='flex flex-col h-screen-with-nav' style={{ 
-      backgroundImage: "url(/cafe.png)" }}>
+      backgroundImage: "url(/cafe.png)", backgroundRepeat: 'no-repeat'}}>
       {/* <h1 className='m-auto text-8xl'>Dialog</h1> */}
       <div className='flex flex-row justify-between'>
           <img className="h-auto max-w-sm rounded-lg shadow-xl dark:shadow-gray-800" src="/barista.png" alt="person 1"></img>
           <img className="h-auto max-w-sm rounded-lg shadow-xl dark:shadow-gray-800" src="/barista.png" alt="person 2"></img>
       </div>
       {/* The dialog text */}
-      <div className='fixed bottom-0 left-0 z-50 my-10 mx-40'>
+      <div className='fixed bottom-0 left-20 z-50 my-10 mx-40 w-8/12'>
         <div className=" m-auto block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Person's Name</h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus quis, dapibus rutrum lorem. Proin accumsan ex nisi, imperdiet mattis metus hendrerit vitae. Phasellus porttitor hendrerit eros, a varius lacus sollicitudin eu. In mi nulla, suscipit et nunc vitae, sagittis pulvinar nisl. Sed convallis egestas ligula, vitae elementum tellus pharetra non. Curabitur ac ultrices nisi, sit amet sodales mauris. In fringilla arcu in aliquet luctus. </p>
+        <div className="font-normal text-gray-700 dark:text-gray-400">
+          <Typewriter text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce urna magna, congue quis metus quis, dapibus rutrum lorem. Proin accumsan ex nisi, imperdiet mattis metus hendrerit vitae. Phasellus porttitor hendrerit eros, a varius lacus sollicitudin eu. In mi nulla, suscipit et nunc vitae, sagittis pulvinar nisl. Sed convallis egestas ligula, vitae elementum tellus pharetra non. Curabitur ac ultrices nisi, sit amet sodales mauris. In fringilla arcu in aliquet luctus" delay={50}></Typewriter>
+        </div>
         </div>
       </div>   
     </div>
