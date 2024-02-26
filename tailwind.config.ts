1bdbd91711de0c1b4f6392e794cb86772293d565
alexis-ng <alexism.ng26@gmail.com>
2024-02-25 23:00:17 -0600

    ahahaa the color has returned

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,42 @@
 import type { Config } from "tailwindcss"
-import ColorPalette from './src/app/_assets/colorpalette'
+import ColorPalette from "./src/app/_assets/colorpalette"
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   prefix: "",
   theme: {
-    // container: {
-    //   center: true,
-    //   padding: "2rem",
-    //   screens: {
-    //     "2xl": "1400px",
-    //   },
-    // },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        ColorPalette, 
+        ...ColorPalette
       },
-      height: {
-        "screen-with-nav": "calc(90vh - 4rem)",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-    }
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
